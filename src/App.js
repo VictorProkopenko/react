73a13643beb7f50093249878a52e24daa6a3178a
viktorpro <victor.prokopenko@gmail.com>
2022-01-13 17:51:25 +0200

    React test git Homework react 1 Classwork react 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Launch from "./components/Launch";
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+        <Launch/>
 
-
-    </div>
+      </div>
   );
 }
 
--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,9 +1,26 @@
-import React from 'react';
 
-export default function launch() {
+import React, {useEffect, useState} from 'react';
+
+export default function Launch() {
+    const [launches, setLaunches] = useState([]);
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => setLaunches(value.filter(value => value.launch_year !== `2020`)));
+    },[]);
+
     return (
         <div>
-
+            <ul>
+                {
+                    launches.map(value => <div key={value.flight_number}>
+                        <li key={value.flight_number}> {value.mission_name} {value.launch_year}</li>
+                        <img  src={value.links.mission_patch_small} alt={'mission_patch_small'}/>
+                    </div>)
+                }
+            </ul>
+            <hr/>
         </div>
     );
-} Launch;
+
+}
