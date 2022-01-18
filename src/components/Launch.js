0892e2d32c75361f0e0bbe04ae24d0d246bafa5e
viktorpro <victor.prokopenko@gmail.com>
2022-01-18 18:00:03 +0200

    React test git Homework react 1 Classwork react 1

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,5 +1,6 @@
 
 import React, {useEffect, useState} from 'react';
+import '../App.css';
 
 import {fetchLaunces} from "../services/launch.services";
 
@@ -11,12 +12,12 @@ export default function Launch() {
     },[]);
 
     return (
-        <div>
+        <div className="launch">
 
                 {
                     launches.map(value => <div key={value.flight_number}>
                         <div key={value.flight_number}> <img  src={value.links.mission_patch_small} alt={'mission_patch_small'}/>
-                            Mission_name: {value.mission_name} Launch year: {value.launch_year} Rocket: {value.rocket.rocket_name} Rocket type:{value.rocket.rocket_type}
+                            Mission_name: {value.mission_name}, Launch year: {value.launch_year}, Rocket: {value.rocket.rocket_name}, Rocket type:{value.rocket.rocket_type}, {value.launch_success} {value.details}
 
                         </div>
                         <a href='https://www.youtube.com/watch?v=v0w9p3U8860'> Video </a>
