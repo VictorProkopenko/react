f786564b68a074f951a7bbf21eb7075cd82f7f90
VictorProkopenko <victor.prokopenko@gmail.com>
2022-01-19 18:06:01 +0200

    Homework react 2

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+
+    }, []);
     return (
         <div>
             Users
