8bfd54f6d1489962a2fb4b32ea8c73ebb473bf80
VictorProkopenko <victor.prokopenko@gmail.com>
2022-01-31 12:18:15 +0200

    ver 2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import {useState, useEffect} from "react";
 import Details from "./components/Details/Details";
 import Posts from "./components/Posts/Posts";
 import Users from "./components/Users/Users";
+import {fetchUser} from "./services/user.service";
 
 function App() {
     const [clickUser, setClickUser] = useState(null)
-    const getUser = (clickUser) => {
-        setClickUser(clickUser);
-
-    };
+    const getUser = (clickUser) => {setClickUser(clickUser); setPosts([])};
     const [count, setCount] = useState(0);
     const increment = () => setCount(parseInt(clickUser.id));
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        if (count) { fetchUser.getPost(count).then(value => setPosts(value)); console.log('fetch',count);}
+    },[count]);
+
     return (
         <div className="App">
             <div className="Users"><Users getUser={getUser}/></div>
             {clickUser && <div className="Users"><Details clickUser={clickUser}/></div>}
-            {clickUser && <div className="Posts"><Posts count={count} onIncrementClick={increment}/></div>}
+            {clickUser && <div className="Posts"><Posts count={count} onIncrementClick={increment} posts={posts}/></div>}
         </div>
     );
 }
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,17 +3,11 @@ import React, {useEffect, useState} from 'react';
 import {fetchUser} from "../../services/user.service";
 
 import Post from "./Post";
-const Posts = ({count, onIncrementClick}) => {
-        const [posts, setPosts] = useState([]);
-        console.log(posts);
-        useEffect(() => {
-            fetchUser.getPost(count).then(value => setPosts(value));
-            console.log('fetch',count);
-        },[count]);
-
+const Posts = ({count, onIncrementClick, posts}) => {
+    console.log(count);
     return (
         <div>
-            <button onClick={onIncrementClick}> Get Posts</button>
+            <button onClick={onIncrementClick}> Get Posts </button>
             <div>
                 {posts.map(post => <Post key={post.id} post={post}/>)}
 
