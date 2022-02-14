80e667fc826a54b7f21f58121226938b84f2debc
VictorProkopenko <victor.prokopenko@gmail.com>
2022-02-14 17:25:51 +0200

    Mini Project v 2.0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import ReactGA from "react-ga";
+import Movies from "./containers/Movies/Movies";
+import Streaming from "./containers/Streaming/Streaming";
+import SideBar from "./containers/UI/SideBar/SideBar";
+import Tv from "./containers/Tv/Tv";
+import Aux from "./hoc/Auxiliary/Auxiliary";
+import NavBar from "./containers/UI/NavBar/NavBar";
+import Home from "./components/Home/Home";
 
-import './App.css';
+ReactGA.initialize("UA-147154395-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  componentDidMount() {}
 
-
-    </div>
-  );
+  render() {
+    return (
+      <Aux>
+        <NavBar />
+        <SideBar />
+        <Route path="/" exact render={(props) => <Home />} />
+        <Route path="/Movies" exact render={(props) => <Movies />} />
+        <Route path="/Streaming" exact render={(props) => <Streaming />} />
+        <Route path="/Tv" exact render={(props) => <Tv />} />
+      </Aux>
+    );
+  }
 }
 
 export default App;
