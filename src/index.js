f536af00423fd7e2f3e062cfc5c889fcb48436c9
VictorProkopenko <victor.prokopenko@gmail.com>
2022-02-17 15:15:36 +0200

    Mini Project v 2.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
- import React from "react";
+import React from "react";
 
-  import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
