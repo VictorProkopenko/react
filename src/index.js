f1b2ad96a91df32e6b1c73d3bcd98e67ea191452
VictorProkopenko <victor.prokopenko@gmail.com>
2022-02-17 15:14:18 +0200

    Mini Project v 2.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+ import React from "react";
+
+  import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
@@ -15,12 +16,14 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-const app = (
+const app = ("");
+
+
   <Provider store={store}>
     <BrowserRouter>
       <Route path="/" component={App} />
     </BrowserRouter>
-  </Provider>
-);
+ </Provider>
+
 
 ReactDOM.render(app, document.getElementById("root"));
